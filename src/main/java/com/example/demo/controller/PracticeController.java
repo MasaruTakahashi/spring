4ae3e39f73414ad67735b943b0600fd23cdc052e
MasaruTakahashi <MasaruTakahashi@users.noreply.github.com>
2019-07-05 17:39:25 +0900

    table作成練習中

--- src/main/java/com/example/demo/controller/PracticeController.java
+++ src/main/java/com/example/demo/controller/PracticeController.java
@@ -1,14 +1,27 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.entity.Task;
+import com.example.demo.entity.TaskRepository;
+
+
 @Controller
 public class PracticeController {
 
+	@Autowired
+	TaskRepository TR;
+
 	@RequestMapping(value = "/")
-	public ModelAndView top(ModelAndView mav) {
+	public ModelAndView top( ModelAndView mav) {
+
+		List<Task> taskList = TR.findAll();
+		mav.addObject("taskList",taskList);
 		mav.setViewName("task");
 		return mav;
 
---
+++ src/main/java/com/example/demo/entity/Task.java
@@ -0,0 +1,57 @@
+package com.example.demo.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="task")
+
+public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String title;
+	private Date create_date;
+	private Date update_date;
+	private int is_Done;
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(Date create_date) {
+		this.create_date = create_date;
+	}
+	public Date getUpdate_date() {
+		return update_date;
+	}
+	public void setUpdate_date(Date update_date) {
+		this.update_date = update_date;
+	}
+	public int getIs_Done() {
+		return is_Done;
+	}
+	public void setIs_Done(int is_Done) {
+		this.is_Done = is_Done;
+	}
+
+
+}
